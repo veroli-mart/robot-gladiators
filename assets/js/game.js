42e14e1e7d373e96cb025c26ff089e2c3962b655
Veronica Martinez <vmartinez6615@gmail.com>
2020-10-28 00:57:04 -0400

    need to fix bugs

--- assets/js/game.js
+++ assets/js/game.js
@@ -5,11 +5,15 @@ var playerMoney = 10;
 
 // You can also log multiple values at once like this
 console.log(playerName, playerAttack, playerHealth);
-
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
+
+
+ var fight = function(enemyNames) {
+//     ... fight funtion statements
+ }
 var fight = function() {
     // Alert plays that they are starting the round
   window.alert("Welcome to Robot Gladiators!");}
@@ -33,7 +37,7 @@ var fight = function() {
     // remove player's health by subtracting the amount set in the enemyAttack variable
     playerHealth = playerHealth - enemyAttack;
     console.log(
-      enemyName + " attacked " + playerName + " . " + playerName + " now has " + playerHealth + " health remaining."
+      enemyNames + " attacked " + playerName + " . " + playerName + " now has " + playerHealth + " health remaining."
     );
   
     // check player's health
@@ -58,10 +62,13 @@ var fight = function() {
     }
 }
  else {
-    fight();
-    }
-}
 
+    }
+
+  }
+  for(var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+}
 
 
 
